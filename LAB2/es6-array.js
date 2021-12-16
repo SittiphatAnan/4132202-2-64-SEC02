bde99049128072f3764ca385ae0824ca60150839
SittiphatAnan <640112418072@bru.ac.th>
2021-12-16 18:18:37 +0700

    LAB3

--- LAB2/es6-array.js
+++ LAB2/es6-array.js
@@ -26,3 +26,49 @@ const newArr2 = [...cars, ...rname]; //spread operator
 console.log(newArr2);
 
 console.log(newArr.toString());
+
+//itteration
+let text = "";
+for(value of rname){
+    text += value + ":"
+}
+console.log(text);
+
+const numbers = [10,20,30]
+numbers.forEach(addNum);
+
+function addNum(value, index, array) {
+    console.log(value += 2);
+}
+
+const ns = numbers.map(addNumMap);
+console.log(ns);
+function addNumMap(value){
+    return value * 2;
+}
+
+const over10 = numbers.filter(over10fn);
+console.log(over10);
+function over10fn(value){
+    return value > 10;
+}
+
+const reOver10 = numbers.reduce(over10fn)
+console.log(reOver10);
+
+//every & some***ตรวจสอบค่าทุกตัว และ บางตัว*****
+const everOver10 = numbers.every(over10fn);
+console.log(everOver10);
+const someOver10 = numbers.some(over10fn)
+console.log(someOver10);
+
+//find & indexOf****
+const resFind = numbers.find(over10fn);
+console.log(resFind);
+const resFindIndex = numbers.indexOf(over10fn);
+console.log(resFindIndex);
+
+//****สร้าง อาร์เรย์ใช้ในการแยกขอ้ความตัวอักษร เช็คข้อความได้
+let alpha = Array.from("UtaBeta123");
+console.log(alpha);
+
